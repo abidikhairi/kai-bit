3182a05468b6a660426a3159ca3616c5f95706b6
Khairi Abidi <abidikhairi.1337@gmail.com>
2025-07-25 07:52:20 +0100

    calculate labels and set loss calculation mode to 'completion'

--- src/kai_bit/datasets/csv_dataset.py
+++ src/kai_bit/datasets/csv_dataset.py
@@ -2,6 +2,8 @@ import os.path as osp
 from typing import Optional, Union
 from datasets import Dataset
 from pytorch_lightning import LightningDataModule
+import torch
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
 
@@ -63,43 +65,66 @@ class CsvDataModule(LightningDataModule):
             .select_columns([self.protein_column, self.prompt_column, self.response_column]) \
             .with_format('torch')
 
+    def _zeros(self, n: int):
+        return [0 for _ in range(n)]
+
     def _collate_fn(self, batch):
         proteins = [item[self.protein_column] for item in batch]
         prompts = [item[self.prompt_column] for item in batch]
         responses = [item[self.response_column] for item in batch]
         
-        protein_inputs = self.protein_tokenizer(proteins, padding=True, truncation=True, return_tensors='pt') # type: ignore
+        protein_inputs = self.protein_tokenizer(proteins, padding=True, return_tensors='pt') # type: ignore
         protein_inputs = {f'protein_{k}': v for k, v in protein_inputs.items()}
         
+        user_message_start = self.language_tokenizer("<|im_start|>user\n") # type: ignore
+        user_message_end = self.language_tokenizer("\n<|im_end|>") # type: ignore
+        assistant_message_start = self.language_tokenizer("<|im_start|>assistant\n") # type: ignore
+        assistant_message_end = self.language_tokenizer("\n<|im_end|>") # type: ignore
         
-        conversations = []
+        all_input_ids = []
+        all_attention_mask = []
+        all_labels = []
         for p, r in zip(prompts, responses):
             if not p.startswith(self.language_tokenizer.protein_token): # type: ignore
-                p = f" {self.language_tokenizer.protein_token} {p}" # type: ignore
-            conversation = [
-                {
-                    'content': p,
-                    'role': 'user'
-                },
-                {
-                    'content': r,
-                    'role': 'assistant'
-                }
-            ]
-            conversations.append(conversation)
+                p = f"{self.language_tokenizer.protein_token} {p}" # type: ignore
+         
+            user_input = self.language_tokenizer(p) # type: ignore
+            user_input['input_ids'] = user_message_start['input_ids'] + user_input['input_ids'] + user_message_end['input_ids'] # type: ignore
+            user_input['attention_mask'] = self._zeros(len(user_message_start['input_ids'])) + user_input['attention_mask'] + self._zeros(len(user_message_end['input_ids'])) # type: ignore
+            
+            assistant_response = self.language_tokenizer(r) # type: ignore
+            assistant_response['input_ids'] = assistant_message_start['input_ids'] + assistant_response['input_ids'] + assistant_message_end['input_ids'] # type: ignore
+            assistant_response['attention_mask'] = self._zeros(len(assistant_message_start['input_ids'])) + assistant_response['attention_mask'] + self._zeros(len(assistant_message_end['input_ids'])) # type: ignore
+
+            input_ids = user_input['input_ids'] + assistant_response['input_ids']
+            attention_mask = user_input['attention_mask'] + assistant_response['attention_mask']
+            
+            input_ids = torch.tensor(input_ids).long()
+            attention_mask = torch.tensor(attention_mask).long()
+            
+            # labels = self._zeros(len(user_input['input_ids'])) + assistant_response['input_ids']
+            
+            # train on user inputs + assistant completion
+            labels = user_input['input_ids'] + assistant_response['input_ids']
+            labels = torch.tensor(labels).long()
+            
+            labels = labels * attention_mask
+            labels[labels == 0] = -100
+            labels[labels == self.language_tokenizer.pad_token_id] = -100 # type: ignore
+            
+            all_input_ids.append(input_ids)
+            all_attention_mask.append(attention_mask)
+            all_labels.append(labels)
         
-        inputs = self.language_tokenizer.apply_chat_template( # type: ignore
-            conversations,
-            return_tensors='pt',
-            padding=True,
-            return_dict=True
-        )
-        
+        all_input_ids = pad_sequence(all_input_ids, batch_first=True, padding_value=self.language_tokenizer.pad_token_id) # type: ignore
+        all_attention_mask = pad_sequence(all_attention_mask, batch_first=True, padding_value=0) # type: ignore
+        all_labels = pad_sequence(all_labels, batch_first=True, padding_value=-100) # type: ignore
         
         batch = {
             **protein_inputs,
-            'input_ids': inputs['input_ids'],
-            'attention_mask': inputs['attention_mask'],
+            'input_ids': all_input_ids,
+            'attention_mask': all_attention_mask,
+            'labels': all_labels
         }
         
         return batch
