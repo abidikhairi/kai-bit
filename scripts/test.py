c59f11ecc240e5a9af51c867b66df89e68cd3684
Khairi Abidi <abidikhairi.1337@gmail.com>
2025-07-26 23:27:54 +0100

    add test.py: run model on test data

--- scripts/test.py
+++ scripts/test.py
@@ -1,41 +1,52 @@
 import argparse
-import os
+from datasets import Dataset
 import torch
-from pytorch_lightning import (
-    Trainer,
-    loggers,
-    callbacks
-)
+from torchmetrics.functional import translation_edit_rate
+from tqdm import tqdm
+import pandas as pd
 
 from kai_bit.systems import BiologicalInstructionTuning
-from kai_bit.config import (
-    ProjectionConfig,
-    OptimizerConfig,
-    TrainingMode
-)
-from kai_bit.datasets import CsvDataModule
-from kai_bit.utils import load_object_from_json
+
 
 torch.set_float32_matmul_precision('medium')
 
 
 def main(args):
-    # MKHNPLVVCLLIICITILTFTLLTRQTLYELRFRDGDKEVAALMACTSR,What is the most probable family for this protein?,Hok/gef cell toxic protein
-    # What is the most probable family for this protein?
-    # Hok/gef cell toxic protein
-    protein = 'MKHNPLVVCLLIICITILTFTLLTRQTLYELRFRDGDKEVAALMACTSR'
-    question = 'From its amino acid pattern, which family is implied?'
-    
+    dataset = Dataset.from_csv(args.input_file)
     model = BiologicalInstructionTuning.load_from_checkpoint(args.ckpt_path).eval()
     
-    output = model.predict_step(protein=protein, user_input=question)
+    data = []
+    
+    for row in tqdm(dataset):
+        protein = row['protein'] # type: ignore
+        question = row['prompt'] # type: ignore
+        
+        output = model.predict_step(protein=protein, user_input=question)
+        
+        predicted_label = output.split('<|annotation_end|>')[0] # type: ignore
+        predicted_label = predicted_label.strip()
+        label = row['answer'].strip()
+        
+        ter = translation_edit_rate([predicted_label], [label]).item() # type: ignore
+        
+        data.append({
+            "sequence": protein,
+            "label": label,
+            "prediction": predicted_label,
+            "translation_edit_rate": ter
+        })
 
-    print(f"Prediction:\n{output}")
+    result_df = pd.DataFrame(data)
+
+    result_df.to_csv(args.output_file, index=False)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--ckpt_path', type=str, required=True, help='Path to checkpoint')
+    parser.add_argument('--input_file', type=str, required=True, help='Path to test file.')
+    parser.add_argument('--output_file', type=str, required=True, help='Path to output file.')
+    parser.add_argument('--ckpt_path', type=str, required=True, help='Path to checkpoint.')
     
     args = parser.parse_args()
     
